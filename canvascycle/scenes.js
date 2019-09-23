eb56a302061f63a17bfc2142e1aa11720ec1c56b
sbentl4 <37422462+sbentl4@users.noreply.github.com>
2019-09-23 14:37:37 -0500

    Canvas-Cycle Inspiration Reverse-engineering

---
+++ canvascycle/scenes.js
@@ -0,0 +1,16 @@
+var scenes = [
+	{
+		name: 'TESTRAMP_2',
+		title: 'Free Test Image Custom',
+	},
+	{
+		name: 'TESTRAMP_2',
+		title: 'Jungle Waterfall - Morning',
+		// sound: 'V08',
+		// maxVolume: 0.25
+	},
+	{
+		name: 'TESTRAMP',
+		title: 'Free Test Image',
+	},
+];
